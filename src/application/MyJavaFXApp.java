1577782e40a44f915b9c88b6574ee3baa55375b3
LuaanNguyen <luanthiennguyen2003@gmail.com>
2024-10-05 21:58:48 -0700

    Add: Initial Documentation 📄

--- src/application/MyJavaFXApp.java
+++ src/application/MyJavaFXApp.java
@@ -6,6 +6,19 @@ import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
+/**
+ * <p> MyJavaFXApp </p>
+ *
+ * <p> Description: Main class for the entire project</p>
+ *
+ * <p> Copyright: Lynn Robert Carter © 2024 </p>
+ *
+ * @author Luan Nguyen, Smit Devrukhkar, Gabriel Clark, Meadow Kubanski, Isabella Paschal
+ *
+ * @version 1.00		2024-10-05 Basic outline of the class
+ *
+ */
+
 public class MyJavaFXApp extends Application {
 	public static void main(String[] args) {
         launch(args);
@@ -14,15 +27,15 @@ public class MyJavaFXApp extends Application {
     
     public void start(Stage primaryStage) {
     	System.out.println("Hello");
-        Button btn = new Button("Don't Press this");
-        btn.setOnAction(e -> System.out.println("You son of a gun"));
+        Button btn = new Button("Press");
+        btn.setOnAction(e -> System.out.println("Button Pressed"));
 
         StackPane root = new StackPane();
         root.getChildren().add(btn);
 
         Scene scene = new Scene(root, 300, 250);
 
-        primaryStage.setTitle("2nd Commit hello");
+        primaryStage.setTitle("Help System");
         primaryStage.setScene(scene);
         primaryStage.show();
     }

72db6dfedef8e8500fad19b53c5623dea10e5927
LuaanNguyen <luanthiennguyen2003@gmail.com>
2024-11-15 14:52:36 -0700

    Added Encryption Class

--- src/module-info.java
+++ src/module-info.java
@@ -1,7 +1,7 @@
 module CSE360_BinaryBabes {
     requires javafx.controls;
-    requires java.sql;
     requires com.h2database;
+    requires org.bouncycastle.lts.prov;
 
     opens application to javafx.graphics, javafx.fxml;
 }

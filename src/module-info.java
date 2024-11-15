c57f39a667bd6fe7b4374016bab0f9a44b2581db
LuaanNguyen <luanthiennguyen2003@gmail.com>
2024-11-15 15:30:43 -0700

    Added bouncy castle

--- src/module-info.java
+++ src/module-info.java
@@ -1,7 +1,8 @@
 module CSE360_BinaryBabes {
     requires javafx.controls;
     requires com.h2database;
-    requires org.bouncycastle.lts.prov;
+    requires org.bouncycastle.provider;
+    requires java.sql;
 
     opens application to javafx.graphics, javafx.fxml;
 }
